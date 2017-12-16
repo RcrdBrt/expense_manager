dc295dff214b0384f472b80568be75999a7f4dbf
Riccardo Berto <riccardobrt@gmail.com>
2017-12-16 16:49:02 +0100

    different entry_ponts format

--- setup.py
+++ setup.py
@@ -22,10 +22,9 @@ setup(
     author='Riccardo Berto',
     author_email='riccardo@rcrdbrt.com',
     packages=find_packages('expense_manager'),
-    entry_points="""
-    [console_scripts]
-    expense_manager = expense_manager.expense_manager:start
-    """,
+    entry_points= {
+        "console_scripts": "expense_manager = expense_manager.expense_manager:start",
+    },
     install_requires=install_requires,
     include_package_data=True,
 )
